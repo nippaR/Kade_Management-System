f2f3203906426fc65df2e34d3288243dc5e6c962
W.A.D.Deshan Wijewardana <dinushadeshan5@gmail.com>
2024-12-01 23:18:56 +0530

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,78 @@
+import React from "react";
+import "./Contact.css";
+import { useNavigate } from "react-router-dom";
+
+const Contact = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="contact-page">
+      <div className="contact-container">
+        <div className="contact-info">
+          <h1 className="contact-title">Contact Us</h1>
+          <div className="contact-details">
+            <p>
+              <strong>PHONE</strong>
+              <br />
+              (+94) 77 456 1111
+            </p>
+            <p>
+              <strong>EMAIL</strong>
+              <br />
+              kademanagement@gmail.com
+            </p>
+            <p>
+              <strong>SOCIAL</strong>
+              <br />
+              <div className="social-icons">
+                <a href="#instagram" className="social-link">
+                  <i className="fa fa-instagram"></i>
+                </a>
+                <a href="#tumblr" className="social-link">
+                  <i className="fa fa-tumblr"></i>
+                </a>
+                <a href="#pinterest" className="social-link">
+                  <i className="fa fa-pinterest"></i>
+                </a>
+                <a href="#facebook" className="social-link">
+                  <i className="fa fa-facebook"></i>
+                </a>
+              </div>
+            </p>
+          </div>
+        </div>
+        <div className="contact-animation">
+          {/* Add a rotating 3D globe or a placeholder */}
+          <img
+            src={require("../assets/globe-placeholder.png")}
+            alt="3D Globe"
+            className="globe-image"
+          />
+        </div>
+      </div>
+      <button className="next-button" onClick={() => navigate("/about")}>
+        NEXT
+      </button>
+
+      {/* Image Section Below */}
+      <div className="image-section">
+        <img
+          src={require("../assets/contact-background.png")}
+          alt="Decorative Globe Background"
+          className="decorative-image"
+        />
+      </div>
+
+      <footer className="footer3">
+        <p>&copy; 2024 KADE Management System. All rights reserved.</p>
+        <div className="social-links">
+          <a href="#facebook">Facebook</a>
+          <a href="#twitter">Twitter</a>
+          <a href="#linkedin">LinkedIn</a>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Contact;
